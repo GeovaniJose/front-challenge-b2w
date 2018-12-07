d88f7edbfc5c1225cc26570bd0e025539f7d88e1
Geovani José <geovani-lol@hotmail.com>
2018-12-07 02:16:38 -0200

    Add Home component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import Header from './components/Header/Header'
+
 import './styles.css'
 
 const App = () => (
-  <h1>HOME</h1>
+  <div className='App'>
+    <Header />
+    <h1>HOME</h1>
+  </div>
 )
 
 export default App
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './styles.css'
+
+const Header = () => (
+  <header id='main-header'><img className='logo' src={require('../../assets/logo.png')} alt='Star Wars' /></header>
+)
+
+export default Header
