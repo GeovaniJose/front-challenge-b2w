a5d37391b7655c2c41734ef874bbafccd589a8df
Geovani José <geovani-lol@hotmail.com>
2018-12-07 05:24:54 -0200

    Add page Home and remove unused dependencie

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,19 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 
 import './styles.css'
 
-const Home = ({ planets }) => (
+const Home = ({ planet, nextPlanet }) => (
   <div className='container'>
     <div className='box'>
-      <h3>{planets.name}</h3>
+      <h3>{planet.name}</h3>
       <div className='content'>
-        <p>Population: {planets.population}</p>
-        <p>Climate: {planets.climate}</p>
-        <p>Terrain: {planets.terrain}</p>
+        <p>Population: {planet.population}</p>
+        <p>Climate: {planet.climate}</p>
+        <p>Terrain: {planet.terrain}</p>
       </div>
-      <p>Featured in {planets.films} films</p>
+      <p>Featured in {planet.films} films</p>
     </div>
-    <button className='next'>NEXT</button>
+    <button className='next' onClick={nextPlanet}>NEXT</button>
   </div>
 )
 
--- src/pages/Home/HomeContainer.js
+++ src/pages/Home/HomeContainer.js
@@ -7,25 +7,30 @@ import Home from './Home'
 
 export default class HomeContainer extends Component {
   state = {
-    planets: []
+    planets: [],
+    planet: {},
+    page: 1
   }
 
   componentDidMount () {
-    NProgress.start()
-    this.loadPlanets()
+    this.chargePage()
   }
 
-  loadPlanets = async () => {
+  chargePage = () => {
+    NProgress.start()
+    this.randomPage()
+  }
+
+  randomPage = async () => {
     try {
-      const response = await api.get('/planets/5c09df546cc1092444f7a035')
+      const response = await api.get('/planets')
 
-      // this.setState({ planets: response.data.docs })
-      this.setState({ planets: response.data })
+      const pages = response.data.pages
+      const page = Math.floor(Math.random() * (pages - 1 + 1) + 1)
 
-      console.log('response', this.state.planets)
+      this.setState({ page })
 
-      document.querySelector('#nprogress .bar').style.backgroundColor = '#007fff'
-      NProgress.done()
+      this.loadPlanets()
     } catch (err) {
       console.log('Erro:', err)
 
@@ -34,9 +39,32 @@ export default class HomeContainer extends Component {
     }
   }
 
+  loadPlanets = async () => {
+    try {
+      const response = await api.get(`/planets?page=${this.state.page}`)
+
+      this.setState({ planets: response.data.docs })
+
+      document.querySelector('#nprogress .bar').style.backgroundColor = '#fff'
+      NProgress.done()
+
+      this.randomPlanet()
+    } catch (err) {
+      console.log('Erro:', err)
+
+      document.querySelector('#nprogress .bar').style.backgroundColor = '#f00'
+      NProgress.done()
+    }
+  }
+
+  randomPlanet = () => {
+    const random = Math.floor(Math.random() * this.state.planets.length)
+    this.setState({ planet: this.state.planets[random] })
+  }
+
   render () {
     return (
-      <Home planets={this.state.planets} />
+      <Home planet={this.state.planet} nextPlanet={this.chargePage} />
     )
   }
 }
