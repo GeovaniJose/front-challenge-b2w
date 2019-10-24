96c1393f938d3764a7c74b2a11113e764742e739
Geovani José <geovani-lol@hotmail.com>
2019-10-23 22:06:43 -0300

    Updates

---
+++ precache-manifest.877ad306c41c0b49aa2871803e320516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f073f350b39a60c55e3ec5d34631766c",
+    "url": "/front-challenge-b2w/index.html"
+  },
+  {
+    "revision": "87ddcdb2c442ff8639e6",
+    "url": "/front-challenge-b2w/static/css/main.5c22dc2a.chunk.css"
+  },
+  {
+    "revision": "991d861b77dd025c9e5e",
+    "url": "/front-challenge-b2w/static/js/2.71c26b6f.chunk.js"
+  },
+  {
+    "revision": "87ddcdb2c442ff8639e6",
+    "url": "/front-challenge-b2w/static/js/main.466eb283.chunk.js"
+  },
+  {
+    "revision": "017a4fa394d356fa90f5",
+    "url": "/front-challenge-b2w/static/js/runtime~main.ad1a859d.js"
+  },
+  {
+    "revision": "7dab29712c638011bd192e20b1d3dd7a",
+    "url": "/front-challenge-b2w/static/media/background-header.7dab2971.jpg"
+  },
+  {
+    "revision": "5d1001864d6c0efe802f0964d8b54325",
+    "url": "/front-challenge-b2w/static/media/logo.5d100186.png"
+  }
+]);
